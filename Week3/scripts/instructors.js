72e33996839544d4028e883f8aa95cbf691929ba
JayIcihmochi <35402378+JayIcihmochi@users.noreply.github.com>
2021-01-27 17:09:29 -0800

    Fixed, Exersize 2 done
    fixed issues with the script to get the right data, and now it proforms as expected.

--- Week3/scripts/instructors.js
+++ Week3/scripts/instructors.js
@@ -1,4 +1,4 @@
-var instructors = '{"instructors": [{"first": "First Name", "middle": "Middle Inital", "last": "Last Name"}, {"first": "front", "middle": "center", "last": "back"}, {"first": "Example", "middle": "Text", "last": "Here"},]}'
+var instructors = '{"instructors": [{"first": "First Name", "middle": "Middle Inital", "last": "Last Name"}, {"first": "front", "middle": "center", "last": "back"}, {"first": "Example", "middle": "Text", "last": "Here"}]}'
 
 function getInstructors() {
     return JSON.parse(instructors);
@@ -12,9 +12,10 @@ function initializeInstructors() {
 function displayInstructors(instructors) {
     var tableRows = "";
     for (i = 0; i < instructors.length; i++) {
-        var id = instructors[i].ID;
-        var email = instructors[i].email;
-        tableRows += "<tr><td>" + id + "</td><td>" + email + "</td></tr>";
+        var firstname = instructors[i].first;
+        var middlename = instructors[i].middle;
+        var lastname = instructors[i].last;
+        tableRows += "<tr><td>" + firstname + "</td><td>" + middlename + "</td><td>" + lastname + "</td></tr>" ;
     }
     document.getElementById("instructorstable").innerHTML = tableRows ;
 }
