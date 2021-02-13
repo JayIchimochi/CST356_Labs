56b5227bbd95d5513585df936802302efb30365a
JayIcihmochi <35402378+JayIcihmochi@users.noreply.github.com>
2021-02-13 13:11:43 -0800

    Updated Student
    Got the studant page to work as well.

--- Week5/webapp/src/components/Students.vue
+++ Week5/webapp/src/components/Students.vue
@@ -3,15 +3,38 @@
         <h1>Student Table</h1>
 
         <table id="studenttable" border="1px solid black">
-            
+            <thead>
+                <tr>
+                    <th>Student ID</th>
+                    <th>Student E-mail</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="student in students" v-bind:key="student">
+                    <td>{{ student.ID }}</td>
+                    <td>{{ student.email }}</td>
+                </tr>
+            </tbody>
         </table>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Student',
-}
+        name: 'StudentsTable',
+        mounted() {
+            this.students = getStudents();
+        },
+        data () {
+            return {
+                students: []
+            }
+        }
+    }
+    function getStudents() {
+        return JSON.parse(students);
+    }
+    var students = '[{"ID": "123456789", "email": "Something@example.com"}, {"ID": "000000000", "email": "ID@domain.com"}]'
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
