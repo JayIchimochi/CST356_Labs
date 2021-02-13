56e66ae9c43e53ca421e4270489fbf370a644331
JayIcihmochi <35402378+JayIcihmochi@users.noreply.github.com>
2021-02-13 11:29:36 -0800

    Exersize 4
    Finished exersizre 4 and migrated the main page to the new format.

---
+++ Week5/webapp/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/image.png">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: lavenderblush;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
