b806f2b5e4cba6c894ba118ff202614ef9e6308f
JayIcihmochi <35402378+JayIcihmochi@users.noreply.github.com>
2021-02-13 11:40:18 -0800

    Added Instructor
    Added Instructor to Vue, but once again, can not confirm it is working quite yet.

---
+++ Week5/webapp/src/components/Instructors.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Instructor Table</h1>
+
+        <table id="instructorstable" border="1px solid black">
+            
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Instructors',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+    font-family: 'Courier New', Courier, monospace;
+    background-color: grey;
+}
+
+#mainheader {
+    font-size: larger;
+    color: lawngreen;
+}
+
+.header-style {
+    color: green;
+}
+
+.tableheading {
+    font-size: large;
+    color: greenyellow;
+}
+</style>
