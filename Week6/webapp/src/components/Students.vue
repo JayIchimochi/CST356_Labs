d5ba957f7ab26191c8a6fc8a1395162c3c5a07bf
JayIcihmochi <35402378+JayIcihmochi@users.noreply.github.com>
2021-02-27 11:31:56 -0800

    got API wokring
    Got API from postman up and working and set up framework for new controllers for students and instructors ready.

---
+++ Week6/webapp/src/components/Students.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <h1>Student Table</h1>
+
+        <table id="studenttable" border="1px solid black">
+            <thead>
+                <tr>
+                    <th>Student ID</th>
+                    <th>Student E-mail</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="student in students" v-bind:key="student">
+                    <td>{{ student.ID }}</td>
+                    <td>{{ student.email }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+        name: 'StudentsTable',
+        mounted() {
+            this.students = getStudents();
+        },
+        data () {
+            return {
+                students: []
+            }
+        }
+    }
+    function getStudents() {
+        return JSON.parse(students);
+    }
+    var students = '[{"ID": "123456789", "email": "Something@example.com"}, {"ID": "000000000", "email": "ID@domain.com"}]'
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+    font-family: 'Courier New', Courier, monospace;
+    background-color: grey;
+}
+
+#mainheader {
+    font-size: larger;
+    color: lawngreen;
+}
+
+.header-style {
+    color: green;
+}
+
+.tableheading {
+    font-size: large;
+    color: greenyellow;
+}
+</style>
